16804f1c825300208cb33446224060436434ac6c
r3dge <regis.harpocrate@gmail.com>
2023-08-26 08:24:46 +0200

    nouvel utilitaire d'effacement

--- src/fillsystemdisk.c
+++ src/fillsystemdisk.c
@@ -1,8 +1,12 @@
+// gcc -o fillsystemdisk fillsystemdisk.c -lm -lncurses -lpthread
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <ncurses.h>
+#include <math.h>
 
 #define NUM_THREADS sysconf(_SC_NPROCESSORS_ONLN)
 #define CHUNK_SIZE 4096
@@ -12,6 +16,25 @@ unsigned long long total_bytes_written = 0;
 unsigned long long total_disk_size = 0;
 int threads_completed = 0;
 
+int getAvailableSpace(){
+    FILE *fp;
+    char buffer[128];
+    int available_space = -1; // Initialize to an invalid value
+
+    fp = popen("df -h . | awk 'NR==2 {print $4}'", "r");
+    if (fp == NULL) {
+        perror("popen");
+        return 1;
+    }
+
+    if (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        sscanf(buffer, "%dG", &available_space);
+    }
+
+    pclose(fp);
+    return available_space;
+}
+
 void *thread_function(void *arg) {
     char filename[20];
     snprintf(filename, sizeof(filename), "thread_file_%ld", (long)arg);
@@ -20,9 +43,11 @@ void *thread_function(void *arg) {
     snprintf(full_path, sizeof(full_path), "./%s", filename);
 
     char command[150]; // Déclaration de la variable "command"
-    snprintf(command, sizeof(command), "dd if=/dev/urandom bs=%d count=1 > %s", CHUNK_SIZE, full_path);
+    
+    snprintf(command, sizeof(command), "dd if=/dev/zero bs=%d > %s", CHUNK_SIZE, full_path);
 
-    if (strlen(full_path) >= sizeof(command)) {
+
+    if (strlen(command) >= sizeof(full_path)) {
         fprintf(stderr, "Path is too long\n");
         pthread_exit(NULL);
     }
@@ -48,6 +73,11 @@ int main() {
         return 1;
     }
 
+    int valeurInitiale;
+    int valeurCourante;
+    double pourcentageInverse;
+    int pourcentageArrondi;
+
     fseek(disk, 0, SEEK_END);
     total_disk_size = ftell(disk);
     fclose(disk);
@@ -61,17 +91,58 @@ int main() {
         }
     }
 
-    printf("Filling disk with random data...\n");
+    valeurInitiale = getAvailableSpace();
+    initscr();
+    start_color();
+    init_pair(1, COLOR_GREEN, COLOR_BLACK);
+
+    printw("\n\n\n\n");
+    printw("\t\t\t\t\tEspace disque à effacer : %d GB\n", valeurInitiale);
+    printw("\n\n");
+    printw("\t\t\t\t\tEffacement du disque en cours...\n\n");
+    printw("\t\t\t\t\t");
+    
+    // initialisation de la barre
+    attron(COLOR_PAIR(1));
+    printw("[");
+    for (int i = 0; i < 29; i++)
+        printw(" ");
+
+    printw("]");
+    refresh();
+    printw("\r");
+    printw("\t\t\t\t\t");
+    printw("[");
+
+    bool termine = false;
+    int avancement_progress_bar = 0;
+    int gap = 0;
 
     while (1) {
         usleep(1000000);  // Sleep for 1 second
+        
+        // recalcul à chaque itération
+        valeurCourante = getAvailableSpace();
+        pourcentageInverse = (1.0 - (double)valeurCourante / valeurInitiale) * 30.0;
+        pourcentageArrondi = (int)floor(pourcentageInverse);
+        if(pourcentageArrondi>avancement_progress_bar){
+            gap = pourcentageArrondi - avancement_progress_bar;
+            for(int i=1;i<=gap;i++){
+                printw("#");
+                refresh();
+                avancement_progress_bar = pourcentageArrondi;
+            }
+            break;
+        }
+        if(avancement_progress_bar >= 30){
+            termine = true;
+            break;
+        }
+
 
         pthread_mutex_lock(&mutex);
-        double percentage_filled = (double)total_bytes_written / total_disk_size * 100;
+        total_bytes_written += CHUNK_SIZE; // Increment the total bytes written
         pthread_mutex_unlock(&mutex);
-
-        clear_console_line();
-        printf("Progress: %.2f%%", percentage_filled);
 
         if (threads_completed >= NUM_THREADS) {
             printf("\nAll threads completed. Cleaning up...\n");
@@ -88,6 +159,6 @@ int main() {
         snprintf(filename, sizeof(filename), "thread_file_%ld", i);
         remove(filename);
     }
-
+    endwin();
     return 0;
 }
