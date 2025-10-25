0855c04cbf85fd1bfc24e07c7c34d91be755463a
r3dge <regis.harpocrate@gmail.com>
2025-10-25 08:37:29 +0200

    ajout de killbit

---
+++ bin/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Installation des dépendances pour killbit"
+
+# sudo apt update
+
+sudo apt install -y build-essential
+sudo apt install -y g++
+sudo apt install -y libcurl4-openssl-dev
+sudo apt install -y curl
+sudo apt install -y hdparm
+sudo apt install -y nvme-cli
+sudo apt install -y inxi
+sudo apt install -y util-linux
+sudo apt install -y coreutils
+sudo apt install -y libcpanel-json-xs-perl
+sudo apt install -y smartmontools
+sudo apt install -y nvme-cli
+
